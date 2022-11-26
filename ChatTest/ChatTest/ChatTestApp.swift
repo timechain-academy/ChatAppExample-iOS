365a0bb2ee3a97cace7eb8920ad6279b9564eb22
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-11-26 01:27:04 -0500

    ChatTest: init

---
+++ ChatTest/ChatTest/ChatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTestApp.swift
+//  ChatTest
+//
+//  Created by git on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
